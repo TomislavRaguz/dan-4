81e4954fa715fde425b8a79b4dc87032b80a80c8
TomislavRaguz <tomislav.raguz@outlook.com>
2017-03-13 16:29:56 +0100

    Add files via upload

---
+++ sedmosmjerka.cpp
@@ -0,0 +1,34 @@
+// sedmosmjerka.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<int> pozicije;
+	bool gotov = false;
+	char rijec[25];
+	char tabla[25];
+	for (int i = 0; i < 25; i++) {
+		cin >> tabla[i];
+	}
+	for (int i = 0; i < 25; i++) {
+		cin >> rijec[i];
+	}
+	START:for (int i = 0; i < 25; i++) {
+		int a = 0;
+		if (rijec[a] == tabla[i]) {
+			i = pozicija.pushback.
+			a++;
+			if(rijec[a]==continue;
+			goto START;
+
+		}
+	}
+	}
+
+
+
