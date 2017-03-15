c5ab98aabf020dbddfaa043c22da44afb4fb637f
TomislavRaguz <tomislav.raguz@outlook.com>
2017-03-15 16:29:27 +0100

    Add files via upload

--- svrlj.cpp
+++ svrlj.cpp
@@ -5,26 +5,69 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <list>
-#include <ctime>
 #include <string>
-
-
 using namespace std;
+
+void prikaz(vector<string> popis);
+void unos(vector<string> popis);
+void pretraga(vector<string> popis);
+
 
 
 void main() {
-	srand(time(NULL));
-	int rand1, rand2;
-	string winner;
-	rand1 = rand() % 6;
-	rand2 = rand() % 6;
-	if (rand1 > rand2) { winner = "Igrac 1 je pobjedio."; }
-	if (rand2 < rand1) { winner = "Igrac 2 je pobjedio.";}
-	else { winner = "Zavrsilo je izjednaceno."; }
-	ofstream output;
-	output.open("primjer.txt", ios::app);
-	output << winner;
-
+	int x,b=0;
+	vector<string> popis;
+	ifstream input;
+	string buffer;
+	input.open("primjer.txt");
+	while (input) {
+		b++;
+		getline(input, buffer);
+		popis.push_back(buffer);
+	}
+	input.close();
+	cout << " 1.prikaz \n 2.unos \n 3.pretraga";
+	cin >> x;
+	switch (x) {
+	case 1:
+		prikaz(popis);
+		break;
+	case 2:
+		unos(popis);
+		break;
+	case 3:
+		pretraga(popis);
+		break;
+	}
+}
+void prikaz(vector<string> popis) {
+	for (int i = 0; i < popis.size(); i++) {
+		cout << popis[i];
+	}
 }
 
+void unos(vector<string> popis) {
+	ofstream output;
+	output.open("primjer.txt", ios::app);
+	string ime, prezime, id, priprema;
+	cout << "Unesite id, ime i prezime: ";
+	cin >> id >> ime >> prezime;
+	priprema = id +" "+ ime +" "+ prezime;
+	output << priprema;
+	output.close();
+}
+
+void pretraga(vector<string> popis) {
+	ifstream input;
+	string indeks;
+	int pozicija;
+	input.open("primjer.txt");
+	string buffer;
+	while (input) {
+		input >> buffer;
+		cout << "Upisite indeks trazenog studenta: ";
+			cin >> indeks;
+			pozicija = indeks.find(buffer);
+			cout << buffer.substr(pozicija);
+	}
+}
